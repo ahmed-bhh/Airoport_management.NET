b8d2934f850e72cd82d9d023916c3ec8f434ff1f
ahmed-bhh <ahmedbelhajhassen22@gmail.com>
2024-02-04 12:50:54 +0100

    changement_path

--- AiroportManagement/AM.UI.Console/obj/Debug/net7.0/AM.UI.Console.AssemblyInfo.cs
+++ AiroportManagement/AM.UI.Console/obj/Debug/net7.0/AM.UI.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AM.UI.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be95375cf3690b38dede3a0569c38e78af0442e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AM.UI.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AM.UI.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
