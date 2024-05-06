fc8d794dc34c2020c12dafa88620072bdecefe1e
ahmedbelhajhassen22@gmail.com <ahmedbelhajhassen22@gmail.com>
2024-05-06 23:47:22 +0100

    kamalt seance 9

---
+++ AiroportManagement/AM.UI.Web/Controllers/FlightController.cs
@@ -0,0 +1,89 @@
+﻿using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Services;
+using AM.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AM.UI.Web.Controllers
+{
+    public class FlightController : Controller
+    {
+       static AMContext ctx=new AMContext();
+       static IUnitOfWork uow=new UnitOfWork(ctx);
+        IserviceFlight sf=new ServiceFlight(uow);
+        // GET: FlightController
+        public ActionResult Index()//wkeft houni seance 9 kamaltha
+        {
+            return View(sf.GetMany());
+        }
+
+        // GET: FlightController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FlightController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FlightController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FlightController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FlightController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FlightController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
